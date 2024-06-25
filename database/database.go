a89861cfd8d84ce057355f8ac241300b97209565
piyush-singh21 <singh.piyush00034@gmail.com>
2024-06-25 11:06:24 +0530

    Added sync in InitDB

--- database/database.go
+++ database/database.go
@@ -3,25 +3,29 @@ package database
 import (
 	"database/sql"
 	"log"
+	"sync"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
+var once sync.Once
 
 func InitDB() {
-	db := "root:root@/event_scheduler"
-	var err error
-	DB, err = sql.Open("mysql", db)
-	if err != nil {
-		log.Fatal("Failed to connect to db \n", err)
-		return
-	}
-	if err := DB.Ping(); err != nil {
-		log.Fatal("Failed to Ping \n", err)
-		return
-	}
-	createTable()
+	once.Do(func() {
+		db := "root:root@/event_scheduler"
+		var err error
+		DB, err = sql.Open("mysql", db)
+		if err != nil {
+			log.Fatal("Failed to connect to db \n", err)
+			return
+		}
+		if err := DB.Ping(); err != nil {
+			log.Fatal("Failed to Ping \n", err)
+			return
+		}
+		createTable()
+	})
 }
 func createTable() {
 	userTable := `
