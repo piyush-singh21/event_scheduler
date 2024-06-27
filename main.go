125246f591d1dcdc2276ec1f57517ee737e7b164
piyush-singh21 <singh.piyush00034@gmail.com>
2024-06-27 14:09:55 +0530

    Added swagger

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	db "event_scheduler/database"
 	routes "event_scheduler/router"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
 	db.InitDB()
 	r := routes.SetupRoute()
+	// ginSwagger.WrapHandler(swaggerFiles.Handler)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run(":8000")
 }
